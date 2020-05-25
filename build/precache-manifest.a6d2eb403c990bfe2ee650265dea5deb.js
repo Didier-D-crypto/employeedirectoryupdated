56af03352057c0292fa7af3c6d9171112aa11577
dalex <delphindidier0@gmail.com>
2020-05-25 01:24:54 -0400

    file updated

---
+++ build/precache-manifest.a6d2eb403c990bfe2ee650265dea5deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4533fc4bccdb8440ecbd32be754171f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe0a0af5f7a14091340",
+    "url": "/static/css/main.0c2f19b1.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "fbe0a0af5f7a14091340",
+    "url": "/static/js/main.5012409a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
